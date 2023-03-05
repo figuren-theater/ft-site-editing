7edc467bce6cc32c9c50fd1f5cf3105504a10770
carstenbach <mail@carsten-bach.de>
2023-03-05 15:01:12 +0100

    Remove setings menu of 'Block Visibility' plugin for users that can not 'manage_sites'

--- inc/block-visibility/namespace.php
+++ inc/block-visibility/namespace.php
@@ -13,9 +13,10 @@ use Figuren_Theater;
 use function Figuren_Theater\get_config;
 
 use function add_action;
-use function is_admin;
+use function current_user_can;
 use function is_network_admin;
 use function is_user_admin;
+use function remove_action;
 
 const BASENAME   = 'block-visibility/block-visibility.php';
 const PLUGINPATH = FT_VENDOR_DIR . '/wpackagist-plugin/' . BASENAME;
@@ -35,12 +36,24 @@ function load_plugin() {
 		return; // early
 
 	// Do only load in "normal" admin view
+	// and public views
 	// Not for:
-	// - public views
 	// - network-admin views
 	// - user-admin views
-	if ( ! is_admin() || is_network_admin() || is_user_admin() )
+	if ( is_network_admin() || is_user_admin() )
 		return;
 	
 	require_once PLUGINPATH;
+	
+	// 'plugins_loaded' is too early for 'current_user_can()'
+	// add_action( 'plugins_loaded', __NAMESPACE__ . '\\post_load_plugin', 11 );
+	add_action( 'admin_menu', __NAMESPACE__ . '\\post_load_plugin', 0 );
+
+}
+
+function post_load_plugin() {
+	if ( current_user_can( 'manage_sites' ) ) {
+		return;
+	}
+	remove_action( 'admin_menu', 'BlockVisibility\\Admin\\add_settings_page' );
 }
